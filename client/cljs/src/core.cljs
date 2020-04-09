beccda55238d5dacb5b6931768a86d2d30ad8e70
prabhanshuguptagit <prabhanshu98@gmail.com>
2020-04-10 00:27:23 +0530

    Add line drawing

--- client/cljs/src/core.cljs
+++ client/cljs/src/core.cljs
@@ -1,20 +1,31 @@
 (ns src.core
   (:require ["paper" :as paper]))
 
-(defn draw []
+(def current-path (atom nil))
+
+#_(defn draw []
   (let [path  (new paper/Path)
         start (new paper/Point 100 100)]
     (set! (.-strokeColor path) "black")
     (.moveTo path start)
-    (.lineTo path (.add start 100 100))
-    (.draw paper/view)))
+    (.lineTo path (.add start 100 130))))
 
-(defn init
-  []
-  (let [canvas (.getElementById js/document "canvas")]
-    (.setup paper canvas)
-    (draw)))
+(defn onMouseDown
+  [event]
+  (reset! current-path (new paper/Path (clj->js {:strokeColor "red"
+                                                :strokeWidth 4
+                                                :strokeJoin  "round"
+                                                :strokeCap   "round"})))
+  (.add @current-path (.-point event)))
+
+(defn onMouseDrag
+  [event]
+  (.add @current-path (.-point event))
+  (.smooth @current-path))
 
 (defn main []
-  (init)
-  (.log js/console "hey"))
+  (let [canvas (.getElementById js/document "canvas")]
+    (.setup paper canvas)
+    (set! (.-onMouseDown paper/view) onMouseDown)
+    (set! (.-onMouseDrag paper/view) onMouseDrag)
+    (set! (.-onMouseUp paper/view) #(.log js/console @current-path))))
