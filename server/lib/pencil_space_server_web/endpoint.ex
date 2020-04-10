e4f6e0f682627d6896ad028556ec1fa9435e6e7d
udit <yudistrange@gmail.com>
2020-04-10 20:28:51 +0530

    Add static index.html

--- server/lib/pencil_space_server_web/endpoint.ex
+++ server/lib/pencil_space_server_web/endpoint.ex
@@ -22,7 +22,7 @@ defmodule PencilSpaceServerWeb.Endpoint do
     at: "/",
     from: :pencil_space_server,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt index.html)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
