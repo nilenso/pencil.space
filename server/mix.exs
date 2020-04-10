042e248765c2973acd71fe51a8dbca3709d6ef38
Akshay Gupta <kitallis@gmail.com>
2020-04-10 23:46:21 +0530

    Lock elixir version to 1.10.2

--- server/mix.exs
+++ server/mix.exs
@@ -5,7 +5,7 @@ defmodule PencilSpaceServer.MixProject do
     [
       app: :pencil_space_server,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "1.10.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
