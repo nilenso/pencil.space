ef3908cc0d6e92d728587a3970d342cb6427f800
udit <yudistrange@gmail.com>
2020-04-10 20:25:12 +0530

    Add phoenix channels to handle connection over ws

--- server/lib/pencil_space_server_web/channels/user_socket.ex
+++ server/lib/pencil_space_server_web/channels/user_socket.ex
@@ -1,8 +1,9 @@
 defmodule PencilSpaceServerWeb.UserSocket do
   use Phoenix.Socket
+  alias PencilSpaceServerWeb.RoomChannel
 
   ## Channels
-  # channel "room:*", PencilSpaceServerWeb.RoomChannel
+  channel "room:*", RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
