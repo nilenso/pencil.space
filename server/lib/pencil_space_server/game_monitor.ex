7e831e837de74903c7f196958930d5b6ae37057c
Akshay Gupta <kitallis@gmail.com>
2020-05-24 02:46:12 +0530

    Add GameSupervisor, GameMonitor and GameState

---
+++ server/lib/pencil_space_server/game_monitor.ex
@@ -0,0 +1,42 @@
+defmodule PencilSpaceServer.GameMonitor do
+  @moduledoc """
+  Game monitor that keeps an eye on the running game.
+  """
+
+  use GenServer
+  alias PencilSpaceServer.{GameMonitor, GameSupervisor, GameState, GameRegistry}
+  defstruct history: [%GameState{}]
+
+  # Client API
+
+  def new(name) do
+    case GenServer.whereis(ref(name)) do
+      nil ->
+        start_game(name)
+      _board ->
+        {:error, :game_exists}
+    end
+  end
+
+  def start_link(options) do
+    GenServer.start_link(__MODULE__, %GameMonitor{}, options)
+  end
+
+  def init(game) do
+    {:ok, game}
+  end
+
+  # Helpers
+
+  def state(%GameMonitor{history: [state | _]}) do
+    state
+  end
+
+  defp start_game(name) do
+    DynamicSupervisor.start_child(GameSupervisor, {__MODULE__, name: ref(name)})
+  end
+
+  defp ref(name) do
+    {:via, Registry, {GameRegistry, name}}
+  end
+end
---
+++ server/lib/pencil_space_server/game_state.ex
@@ -0,0 +1,8 @@
+defmodule PencilSpaceServer.GameState do
+  @moduledoc """
+  Game state for keeping a log of the state changes.
+  """
+
+  defstruct id: 1,
+            host: :bot
+end
