3f1d075fa73359ff93c3b6a17bd5e335517d0430
prabhanshuguptagit <prabhanshu98@gmail.com>
2020-05-31 03:22:29 +0530

    Remove cljs compile files

--- client/cljs/static/assets/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-src.core.init();
--- client/cljs/static/assets/cljs-runtime/src.db.js
+++
@@ -1,5 +0,0 @@
-goog.provide('src.db');
-goog.require('cljs.core');
-src.db.default$ = new cljs.core.PersistentArrayMap(null, 8, [new cljs.core.Keyword(null,"app-name","app-name",-268811251),"pencil.space",new cljs.core.Keyword(null,"id","id",-1388402092),null,new cljs.core.Keyword(null,"game","game",-441523833),null,new cljs.core.Keyword(null,"participants","participants",673603367),cljs.core.PersistentArrayMap.EMPTY,new cljs.core.Keyword(null,"nick-name","nick-name",-706954142),null,new cljs.core.Keyword(null,"avatar","avatar",-1607499307),null,new cljs.core.Keyword(null,"chat-history","chat-history",912746146),cljs.core.PersistentVector.EMPTY,new cljs.core.Keyword(null,"current-route","current-route",2067529448),null], null);
-
-//# sourceMappingURL=src.db.js.map
